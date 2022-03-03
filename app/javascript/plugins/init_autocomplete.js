187345996d0ffe09ed4d61e6cc1803c14f3ab684
Hoi Yan Lam <iamhy10421@gmail.com>
2022-03-03 15:39:01 +0100

    offer form style

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -3,6 +3,7 @@ import places from 'places.js';
 const initAutocomplete = () => {
   const addressInput = document.getElementById('offer_address');
   if (addressInput) {
+    console.log('hello');
     places({ container: addressInput });
   }
 };
