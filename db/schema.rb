c66fdcf26b335b63528c8e3c50647aad63e5e1d5
Hoi Yan Lam <iamhy10421@gmail.com>
2022-03-04 15:57:51 +0100

    status of the offer & booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_03_115700) do
+ActiveRecord::Schema.define(version: 2022_03_04_142852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,11 +44,11 @@ ActiveRecord::Schema.define(version: 2022_03_03_115700) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.string "status"
     t.bigint "user_id", null: false
     t.bigint "offer_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "status", default: true
     t.index ["offer_id"], name: "index_bookings_on_offer_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
